6637a4c8c8bb44be20a4922468364665344fbdc5
Tiago <tiago.asp.net@gmail.com>
2015-10-06 08:29:25 -0300

    I built GitHub Issues that gets an url and it returns json data with the issues

---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,23 @@
+defmodule Issues.GithubIssues do 
+	@user_agent [ {"User-agent", "tiago.asp.net@gmail.com"} ]	
+
+	def fetch(user, project) do 
+		issues_url(user, project)
+		|> HTTPoison.get(@user_agent)
+		|> handle_response
+	end	
+
+	def issues_url(user, project) do 
+		"https://api.github.com/repos/#{user}/#{project}/issues"
+	end
+
+	#def handle_response(%{status_code: 200, body: body}), do: {:ok, body}	
+	#def handle_response(%{status_code: ___, body: body}), do: {:error, body}
+
+	def handle_response({:error, %HTTPoison.Error{reason: error}}), do: {:error, error}
+
+  	def handle_response({:ok, %HTTPoison.Response{status_code: status, body: body}}) 
+  	when status >= 400, do: {:error, :jsx.decode(body)}
+
+  	def handle_response({:ok, %HTTPoison.Response{body: body}}), do: {:ok, :jsx.decode(body)}		
+end 						
