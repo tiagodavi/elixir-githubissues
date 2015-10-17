0171870a35e0284c6a4735cfc9737a26f01fa182
Tiago <tiago.asp.net@gmail.com>
2015-10-16 21:19:57 -0300

    I put more functions and tests to improve that code

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -36,8 +36,12 @@ defmodule Issues.CLI do
 		System.halt(0)
 	end
 
-	def process({user, project, _count}) do 
-		Issues.GithubIssues.fecth(user, project)
+	def process({user, project, count}) do 
+		Issues.GithubIssues.fetch(user, project)
+		|> decode_response
+		|> convert_to_list_of_hashdicts
+		|> sort_into_ascending_order
+		|> Enum.take(count)
 	end
 
 	def decode_response({:ok, body}), do: body
@@ -49,6 +53,10 @@ defmodule Issues.CLI do
 	def convert_to_list_of_hashdicts(list) do
 		list
 		|> Enum.map(&Enum.into(&1, HashDict.new))
+	end
+	def sort_into_ascending_order(list_of_issues) do
+		Enum.sort list_of_issues,
+			fn i1, i2 -> i1["created_at"] <= i2["created_at"] end
 	end
 
 end
--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,4 +1,5 @@
 defmodule Issues.GithubIssues do 
+	@github_url Application.get_env(:issues, :github_url)	
 	@user_agent [ {"User-agent", "tiago.asp.net@gmail.com"} ]	
 
 	def fetch(user, project) do 
@@ -8,7 +9,7 @@ defmodule Issues.GithubIssues do
 	end	
 
 	def issues_url(user, project) do 
-		"https://api.github.com/repos/#{user}/#{project}/issues"
+		"#{@github_url}/repos/#{user}/#{project}/issues"
 	end
 
 	#def handle_response(%{status_code: 200, body: body}), do: {:ok, body}	
